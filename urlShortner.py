c48923d4cf00b442e17c16b9127a50805096175f
Md Farhatus Sium <252-35-001@diu.edu.bd>
2026-01-25 00:58:42 +0600

    Remove implementations for password strength checker, weather application, and URL shortener

--- urlShortner.py
+++
@@ -1,8 +0,0 @@
-import pyshorteners
-
-long_url = input("Enter the URL to shorten: ")
-
-shortener = pyshorteners.Shortener()
-short_url = shortener.tinyurl.short(long_url)
-
-print("Short URL:", short_url)
